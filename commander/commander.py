dd2fb68492a7681026ca54be3b01737acd0bb68f
j-fuentes <jotaefe555555@gmail.com>
2015-05-26 12:37:26 +0200

    Parsers reordened

--- commander/commander.py
+++ commander/commander.py
@@ -24,6 +24,7 @@ def not_found():
 
 builder_ns = api.namespace('builder', description='Building related operations')
 
+## Response models
 contextListModel = api.model('ContextList', builderSchemas.context_process_list_response)
 contextInfoModel = api.model('ContextInfo', builderSchemas.context_basic_status_response)
 contextDetailModel = api.model('ContextDetail', builderSchemas.context_detailed_status_response)
@@ -31,27 +32,37 @@ imageListModel = api.model('ImageList', builderSchemas.build_process_list_respon
 imageInfoModel = api.model('ImageInfo', builderSchemas.build_basic_status_response)
 imageDetailModel = api.model('ImageDetail', builderSchemas.build_detailed_status_response)
 
+## Arguments models
+contextArgs = api.parser()
+contextArgs.add_argument('contextName', help='Optional name for the context', location='form')
+contextArgs.add_argument('puppetfile', type=FileStorage, help='Puppetfile that indicates the puppet modules needed in the context' , location='files')
+
+buildArgs = api.parser()
+buildArgs.add_argument('imageName', help='Desired image name.', location='form')
+buildArgs.add_argument('contextReference', help='Reference (context token) to the context where the image will be build. If not set it will be build in a new and empty context.', location='form')
+buildArgs.add_argument('dockerfile', type=FileStorage, help='Base image dockerfile' , location='files')
+buildArgs.add_argument('puppetmanifest', type=FileStorage, help='Puppet manifest that contains the service definition for the image.' , location='files')
 
 @builder_ns.route('/contexts')
 class ContextService(Resource):
+
     @api.doc(description='Retrieve list of contexts.')
     @api.response(500, 'Error processing the request', errorResponseModel)
     @api.response(201, 'OK', contextListModel)
     def get(self):
         return not_implemented()
 
-    contextArgs = api.parser()
-    contextArgs.add_argument('contextName', help='Optional name for the context', location='form')
-    contextArgs.add_argument('puppetfile', type=FileStorage, help='Puppetfile that indicates the puppet modules needed in the context' , location='files')
     @api.doc(description='Create new context.', parser=contextArgs)
     @api.response(500, 'Error processing the request', errorResponseModel)
     @api.response(201, 'Created', contextInfoModel)
     def post(self):
         return not_implemented()
 
+
 @builder_ns.route('/contexts/<token>')
 @api.doc(params={'token': 'Token that identifies the context.'})
 class Context(Resource):
+
     @api.doc(description='Get information about a context.' )
     @api.response(500, 'Error processing the request', errorResponseModel)
     @api.response(404, 'Not found', errorResponseModel)
@@ -69,18 +80,13 @@ class Context(Resource):
 
 @builder_ns.route('/images')
 class BuildService(Resource):
+
     @api.doc(description='Retrieve list of processes.')
     @api.response(500, 'Error processing the request', errorResponseModel)
     @api.response(201, 'OK', imageListModel)
     def get(self):
         return not_implemented()
 
-
-    buildArgs = api.parser()
-    buildArgs.add_argument('imageName', help='Desired image name.', location='form')
-    buildArgs.add_argument('contextReference', help='Reference (context token) to the context where the image will be build. If not set it will be build in a new and empty context.', location='form')
-    buildArgs.add_argument('dockerfile', type=FileStorage, help='Base image dockerfile' , location='files')
-    buildArgs.add_argument('puppetmanifest', type=FileStorage, help='Puppet manifest that contains the service definition for the image.' , location='files')
     @api.doc(description='Start a build process.', parser=buildArgs)
     @api.response(500, 'Error processing the request', errorResponseModel)
     @api.response(201, 'Created', imageInfoModel)
@@ -91,6 +97,7 @@ class BuildService(Resource):
 @builder_ns.route('/images/<token>')
 @api.doc(params={'token': 'Token that identifies the building process.'})
 class BuildProcess(Resource):
+
     @api.doc(description='Get information about a build process.')
     @api.response(500, 'Error processing the request', errorResponseModel)
     @api.response(404, 'Not found', errorResponseModel)
@@ -110,9 +117,11 @@ class BuildProcess(Resource):
 
 cluster_ns = api.namespace('cluster', description='Cluster related operations')
 
+## Response models
 clusterInfoModel = api.model('ClusterInfo', clusterSchemas.cluster_basic_status_response)
 clusterDetailModel = api.model('ClusterDetail', clusterSchemas.cluster_detailed_status_response)
 
+## Arguments models
 machineArgs = api.parser()
 machineArgs.add_argument('hostname', help='Machine hostname or IP where is accesible.', location='form')
 machineArgs.add_argument('port', type=int, help='SSH port.', location='form')
@@ -123,22 +132,24 @@ machineArgs.add_argument('password', help='In case of user-password credentials:
 
 @cluster_ns.route('')
 class ClusterService(Resource):
+
     @api.doc(description='Create a new swarm cluster.', parser=machineArgs)
     @api.response(500, 'Error processing the request', errorResponseModel)
     @api.response(201, 'Created', clusterInfoModel)
     def post(self):
         return not_implemented()
 
+
 @cluster_ns.route('/<token>')
 @api.doc(params={'token': 'Token that identifies the docker swarm cluster.'})
 class ClusterInstance(Resource):
+
     @api.doc(description='Add a new machine to a swarm cluster.', parser=machineArgs)
     @api.response(500, 'Error processing the request', errorResponseModel)
     @api.response(404, 'Not found', errorResponseModel)
     @api.response(200, 'Created', clusterInfoModel)
     def put(self):
         return not_implemented()
-
 
     @api.doc(description='Get information about a docker swarm cluster.')
     @api.response(500, 'Error processing the request', errorResponseModel)
@@ -159,30 +170,35 @@ class ClusterInstance(Resource):
 
 composer_ns = api.namespace('composer', description='Composer related operations')
 
+## Response models
 composerInfoModel = api.model('ComposerInfo', composerSchemas.composer_basic_status_response)
 composerDetailModel = api.model('ComposerDetail', composerSchemas.composer_detailed_status_response)
 
+## Arguments models
+composerArgs = api.parser()
+composerArgs.add_argument('clusterToken', help='Reference to the cluster (token used in the \'cluster\' operations).', location='form')
+composerArgs.add_argument('composefile', type=FileStorage, help='Docker-compose.yml file that specifies how to compose the service.' , location='files')
+
 @composer_ns.route('')
 class ComposerService(Resource):
-    composerArgs = api.parser()
-    composerArgs.add_argument('clusterToken', help='Reference to the cluster (token used in the \'cluster\' operations).', location='form')
-    composerArgs.add_argument('composefile', type=FileStorage, help='Docker-compose.yml file that specifies how to compose the service.' , location='files')
+
     @api.doc(description='Instance a container based service by deploying and linking the containers defined in the docker-compose.yml.', parser=composerArgs)
     @api.response(500, 'Error processing the request', errorResponseModel)
     @api.response(201, 'Created', composerInfoModel)
     def post(self):
         return not_implemented()
 
+
 @composer_ns.route('/<token>')
 @api.doc(params={'token': 'Token that identifies the docker composition'})
 class ComposerDeployment(Resource):
+
     @api.doc(description='Get information about a docker container composition.')
     @api.response(500, 'Error processing the request', errorResponseModel)
     @api.response(404, 'Not found', errorResponseModel)
     @api.response(200, 'OK', composerDetailModel)
     def get(self, token):
         return not_implemented()
-
 
     @api.doc(description='Destroy the container composition and the related data.')
     @api.response(500, 'Error processing the request', errorResponseModel)
