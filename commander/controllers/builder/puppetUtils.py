138746fba743c750bcfaabc3a1182e0a27512d28
j-fuentes <jotaefe555555@gmail.com>
2015-05-27 18:07:00 +0200

    Changed name puppetOperations -> puppetUtils

---
+++ commander/controllers/builder/puppetUtils.py
@@ -0,0 +1,12 @@
+def checkPuppetfile():
+    '''
+    Checks puppefile syntax. Returns True if correct syntax or False if not.
+    '''
+    pass
+
+def buildContext():
+    '''
+    Creates a context in the filesystem ('/var/builder/contexts/<contextToken>'),
+    stores the puppetfile and downloads the puppet modules.
+    '''
+    pass
