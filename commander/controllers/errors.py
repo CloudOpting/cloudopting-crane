f7634343ff8849f48bd7ba26d37a786da71ceb3a
j-fuentes <jotaefe555555@gmail.com>
2015-06-02 08:19:48 +0200

    Added errors for controllers

---
+++ commander/controllers/errors.py
@@ -0,0 +1,26 @@
+class ControllerError(Exception):
+    '''
+    Generic error inside controller
+    '''
+    def __init__(self, message=None, errors=None, type='generic'):
+        super(ControllerError, self).__init__(message)
+        self.errors = errors
+        self.message = message
+        self.type = type
+
+    def getResponse(self):
+        return {'error': self.type, 'description': self.message}
+
+class OperationError(ControllerError):
+    '''
+    Error while doing any operation requested.
+    '''
+    def __init__(self, message=None, errors=None):
+        super(OperationError, self).__init__(message, errors, 'operationError')
+
+class NotFoundError(ControllerError):
+    '''
+    Resource not found
+    '''
+    def __init__(self, message=None, errors=None):
+        super(NotFoundError, self).__init__(message, errors, 'notFound')
