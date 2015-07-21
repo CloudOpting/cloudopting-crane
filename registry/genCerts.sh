f5736abc0cdee3e7c34596e1aaf8c52a4cb20321
j-fuentes <jotaefe555555@gmail.com>
2015-07-21 11:26:22 +0200

    Basic configuration to run registry in private environment

---
+++ registry/genCerts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "The main field you must introduce is the CN (Common Name). It must be something like 'cloudopting.eu' and must be resolvable by someone who wants to interact with the registry."
+echo "This must generate a folder (named as 'certs') and two files inside (domain.key and 'domain.crt')"
+mkdir -p certs && openssl req -newkey rsa:4096 -nodes -sha256 -keyout certs/domain.key -x509 -days 365 -out certs/domain.crt
