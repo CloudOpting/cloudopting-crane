bacaf75a0c50833b469b9932510b8b1e8bf20a5e
j-fuentes <jotaefe555555@gmail.com>
2015-07-21 11:56:43 +0200

    launcher skell

---
+++ launcher.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+## Launcher for CloudOpting-Crane
+
+# add parameters to select:
+# - domain
+# - disable registry and remote deploy
+# - debug mode
+
+# check:
+# - local docker running
+# - registry -> install if not installed, reconfigure if needed.
+# - run commander, do the built-in check and parse response
