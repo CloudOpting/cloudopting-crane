c2050dcdf6d6700de4fa3e0b3a731c5889dbdb1d
j-fuentes <jotaefe555555@gmail.com>
2015-08-27 15:01:42 +0200

    Registry certificates

--- genCerts.sh
+++ genCerts.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Definitions
-export CA_PASSWORD=p4ssw0rd
 export ENGINE_HOSTNAME=coengine
 export CLIENT_HOSTNAME=commander
-export ENGINE_BASE_DIR=engine/certs
+export CA_PASSWORD=p4ssw0rd
+export ENGINE_CERTS_DIR=engine/certs
+
+export REGISTRY_HOSTNAME=coregistry
+export REGISTRY_AUTH_DIR=registry/auth
+export REGISTRY_CERTS_DIR=registry/auth
+export REGISTRY_DEF_USER=reguser
+export REGISTRY_DEF_USER_PASS=s3cr3tp4ssw0rd
 
 
 # Colors
@@ -14,57 +20,63 @@ BINFO='\033[1;33m'
 SUCC='\033[0;36m'
 UINFO='\033[0;37m'
 ELEM='\033[1;34m'
+OP='\033[2;33m'
 NC='\033[0m' # No Color
 
-# Gen engine certificates
-mkdir -p ${ENGINE_BASE_DIR}
+# ENGINE CERTIFICATES
+mkdir -p ${ENGINE_CERTS_DIR}
 ## CA [ca.pem, ca-key.pem]
 printf "${INFO}Generating CA key and cert...${NC}\n\n"
-openssl genrsa -aes256 -passout env:CA_PASSWORD -out ${ENGINE_BASE_DIR}/ca-key.pem 4096
-openssl req -new -x509 -passin env:CA_PASSWORD -days 365 -key ${ENGINE_BASE_DIR}/ca-key.pem \
-  -sha256 -out ${ENGINE_BASE_DIR}/ca.pem -subj "/C=ES/ST=Seville/L=Seville/O=CloudOpting/OU=IT/CN=$ENGINE_HOSTNAME"
+printf "${OP}"
+openssl genrsa -aes256 -passout env:CA_PASSWORD -out ${ENGINE_CERTS_DIR}/ca-key.pem 4096
+openssl req -new -x509 -passin env:CA_PASSWORD -days 365 -key ${ENGINE_CERTS_DIR}/ca-key.pem \
+  -sha256 -out ${ENGINE_CERTS_DIR}/ca.pem -subj "/C=ES/ST=Seville/L=Seville/O=CloudOpting/OU=IT/CN=$ENGINE_HOSTNAME"
+printf "${NC}"
 printf "\n${SUCC}Got ${ELEM}ca-key.pem${SUCC} and ${ELEM}ca.pem${SUCC} .${NC}\n\n\n"
 
 
 ## Signed server key [server-cert.pem, server-key.pem]
 printf "${INFO}Generating server keys...${NC}\n\n"
-openssl genrsa -out ${ENGINE_BASE_DIR}/server-key.pem 4096
-openssl req -subj "/CN=$ENGINE_HOSTNAME" -sha256 -new -key ${ENGINE_BASE_DIR}/server-key.pem -out ${ENGINE_BASE_DIR}/server.csr
-echo subjectAltName = IP:127.0.0.1 > ${ENGINE_BASE_DIR}/extfile.cnf
-openssl x509 -req -days 365 -sha256 -in ${ENGINE_BASE_DIR}/server.csr -CA ${ENGINE_BASE_DIR}/ca.pem -CAkey ${ENGINE_BASE_DIR}/ca-key.pem \
-  -CAcreateserial -out ${ENGINE_BASE_DIR}/server-cert.pem -extfile ${ENGINE_BASE_DIR}/extfile.cnf -passin env:CA_PASSWORD
-rm ${ENGINE_BASE_DIR}/extfile.cnf
-rm -v ${ENGINE_BASE_DIR}/server.csr
+printf "${OP}"
+openssl genrsa -out ${ENGINE_CERTS_DIR}/server-key.pem 4096
+openssl req -subj "/CN=$ENGINE_HOSTNAME" -sha256 -new -key ${ENGINE_CERTS_DIR}/server-key.pem -out ${ENGINE_CERTS_DIR}/server.csr
+echo subjectAltName = IP:127.0.0.1 > ${ENGINE_CERTS_DIR}/extfile.cnf
+openssl x509 -req -days 365 -sha256 -in ${ENGINE_CERTS_DIR}/server.csr -CA ${ENGINE_CERTS_DIR}/ca.pem -CAkey ${ENGINE_CERTS_DIR}/ca-key.pem \
+  -CAcreateserial -out ${ENGINE_CERTS_DIR}/server-cert.pem -extfile ${ENGINE_CERTS_DIR}/extfile.cnf -passin env:CA_PASSWORD
+rm ${ENGINE_CERTS_DIR}/extfile.cnf
+rm -v ${ENGINE_CERTS_DIR}/server.csr
+printf "${NC}"
 printf "\n${SUCC}Got ${ELEM}server-cert.pem${SUCC} and ${ELEM}server-key.pem${SUCC} .${NC}\n\n\n"
 
 
 
 ## Signed client key [client-cert.pem, client-key.pem]
 printf "${INFO}Generating client keys...${NC}\n\n"
-openssl genrsa -out ${ENGINE_BASE_DIR}/client-key.pem 4096
-openssl req -subj "/CN=$CLIENT_HOSTNAME" -new -key ${ENGINE_BASE_DIR}/client-key.pem -out ${ENGINE_BASE_DIR}/client.csr
-echo extendedKeyUsage = clientAuth > ${ENGINE_BASE_DIR}/extfile.cnf
-openssl x509 -req -days 365 -sha256 -in ${ENGINE_BASE_DIR}/client.csr -CA ${ENGINE_BASE_DIR}/ca.pem -CAkey ${ENGINE_BASE_DIR}/ca-key.pem \
-  -CAcreateserial -out ${ENGINE_BASE_DIR}/client-cert.pem -extfile ${ENGINE_BASE_DIR}/extfile.cnf -passin env:CA_PASSWORD
-rm ${ENGINE_BASE_DIR}/extfile.cnf
-rm -v ${ENGINE_BASE_DIR}/client.csr
+printf "${OP}"
+openssl genrsa -out ${ENGINE_CERTS_DIR}/client-key.pem 4096
+openssl req -subj "/CN=$CLIENT_HOSTNAME" -new -key ${ENGINE_CERTS_DIR}/client-key.pem -out ${ENGINE_CERTS_DIR}/client.csr
+echo extendedKeyUsage = clientAuth > ${ENGINE_CERTS_DIR}/extfile.cnf
+openssl x509 -req -days 365 -sha256 -in ${ENGINE_CERTS_DIR}/client.csr -CA ${ENGINE_CERTS_DIR}/ca.pem -CAkey ${ENGINE_CERTS_DIR}/ca-key.pem \
+  -CAcreateserial -out ${ENGINE_CERTS_DIR}/client-cert.pem -extfile ${ENGINE_CERTS_DIR}/extfile.cnf -passin env:CA_PASSWORD
+rm ${ENGINE_CERTS_DIR}/extfile.cnf
+rm -v ${ENGINE_CERTS_DIR}/client.csr
+printf "${NC}"
 printf "\n${SUCC}Got ${ELEM}client-cert.pem${SUCC} and ${ELEM}client-key.pem${SUCC} .${NC}\n\n\n"
 
 ## How to set client certificates
 printf "${BINFO}-----------------------------------------------------------------------------------------------------------------------${NC}\n"
 printf "${BINFO}  These notes help you to configure manually the docker engine and docker client with the generated certificates.${NC}\n"
-printf         "  If your plan is to use the elements in this project (${ELEM}engine${BINFO} and ${ELEM}commander${BINFO}), you can ignore them.${NC}\n"
+printf "${BINFO}  If your plan is to use the elements in this project (${ELEM}engine${BINFO} and ${ELEM}commander${BINFO}), you can ignore them.${NC}\n"
 printf "${BINFO}-----------------------------------------------------------------------------------------------------------------------${NC}\n"
 
-
-printf "\n${BINFO}BONUS:${INFO} How to set client certificates:${NC}\n"
-printf "\t${UINFO}- Copy ${ELEM}${ENGINE_BASE_DIR}/client-cert.pem${UINFO} to ${ELEM}~/.docker/cert.pem${UINFO}.${NC}\n"
-printf "\t${UINFO}- Copy ${ELEM}${ENGINE_BASE_DIR}/client-key.pem${UINFO} to ${ELEM}~/.docker/key.pem${UINFO}.${NC}\n"
-printf "\t${UINFO}- Copy ${ELEM}${ENGINE_BASE_DIR}/ca.pem${UINFO} to ${ELEM}~/.docker/ca.pem${UINFO}.${NC}\n"
+printf "\n${BINFO}NOTE I:${INFO} How to set client certificates:${NC}\n"
+printf "\t${UINFO}- Copy ${ELEM}${ENGINE_CERTS_DIR}/client-cert.pem${UINFO} to ${ELEM}~/.docker/cert.pem${UINFO}.${NC}\n"
+printf "\t${UINFO}- Copy ${ELEM}${ENGINE_CERTS_DIR}/client-key.pem${UINFO} to ${ELEM}~/.docker/key.pem${UINFO}.${NC}\n"
+printf "\t${UINFO}- Copy ${ELEM}${ENGINE_CERTS_DIR}/ca.pem${UINFO} to ${ELEM}~/.docker/ca.pem${UINFO}.${NC}\n"
 printf "\t${UINFO}- Set permissions ${ELEM}0600${UINFO} to the previous files.${NC}\n\n"
 printf "  ${INFO}The client must be able to resolve the name (which is configured in this script as ${ELEM}ENGINE_HOSTNAME=${ENGINE_HOSTNAME}${NC}) of the docker engine.${NC}\n"
 
-printf "\n${BINFO}BONUS II:${INFO} How to configure environment to allow docker client communicate with docker engine:${NC}\n"
+printf "\n${BINFO}NOTE II:${INFO} How to configure environment to allow docker client communicate with docker engine:${NC}\n"
 printf "\t${UINFO}Set the following environment variables in the client machine:\n"
 printf "\t\t${UINFO}· DOCKER_TLS_VERIFY=1\n"
 printf "\t\t${UINFO}· DOCKER_CERT_PATH=/home/user/.docker\n"
@@ -72,10 +84,10 @@ printf "\t\t${UINFO}· DOCKER_HOST=tcp://${ENGINE_HOSTNAME}:4243\n"
 printf "\t\t${UINFO}Assuming 4243 is the port where the docker daemon will be listening, ${ELEM}user${UINFO} is the user who execute docker client\n"
 printf "\t\tand you can resolve ${ELEM}${ENGINE_HOSTNAME}${UINFO} to the address where the engine is running.${NC}\n"
 
-printf "\n${BINFO}BONUS III:${INFO} How to configure certificates in engine:${NC}\n"
-printf "\t${UINFO}- Copy ${ELEM}${ENGINE_BASE_DIR}/server-cert.pem${UINFO} to ${ELEM}/root/.docker/cert.pem${UINFO}.${NC}\n"
-printf "\t${UINFO}- Copy ${ELEM}${ENGINE_BASE_DIR}/server-key.pem${UINFO} to ${ELEM}/root/.docker/key.pem${UINFO}.${NC}\n"
-printf "\t${UINFO}- Copy ${ELEM}${ENGINE_BASE_DIR}/ca.pem${UINFO} to ${ELEM}/root/.docker/ca.pem${UINFO}.${NC}\n"
+printf "\n${BINFO}NOTE III:${INFO} How to configure certificates in engine:${NC}\n"
+printf "\t${UINFO}- Copy ${ELEM}${ENGINE_CERTS_DIR}/server-cert.pem${UINFO} to ${ELEM}/root/.docker/cert.pem${UINFO}.${NC}\n"
+printf "\t${UINFO}- Copy ${ELEM}${ENGINE_CERTS_DIR}/server-key.pem${UINFO} to ${ELEM}/root/.docker/key.pem${UINFO}.${NC}\n"
+printf "\t${UINFO}- Copy ${ELEM}${ENGINE_CERTS_DIR}/ca.pem${UINFO} to ${ELEM}/root/.docker/ca.pem${UINFO}.${NC}\n"
 printf "\t${UINFO}- Add the following options to ${ELEM}DOCKER_OPTS${UINFO} (the location of ${ELEM}DOCKER_OPTS${UINFO} depends on the linux distribution):\n"
 printf "\t\t${ELEM} --tlsverify --tlscacert=/root/.docker/ca.pem --tlscert=/root/.docker/cert.pem --tlskey=/root/.docker/key.pem${NC}\n"
 
@@ -83,7 +95,37 @@ printf "${BINFO}----------------------------------------------------------------
 
 
 
-# Set engine cetificates
 
-# Gen registry certificates
-# Set registry certificates
+# REGISTRY CERTIFICATES
+## Create htpasswd with user for basic autentication
+printf "${OP}"
+mkdir -p ${REGISTRY_AUTH_DIR}
+htpasswd -Bbn ${REGISTRY_DEF_USER} ${REGISTRY_DEF_USER_PASS} > ${REGISTRY_AUTH_DIR}/htpasswd
+printf "${NC}"
+
+## Create domain certificates for registry
+printf "${OP}"
+mkdir -p ${REGISTRY_CERTS_DIR}
+openssl req -subj "/CN=$REGISTRY_HOSTNAME" -newkey rsa:4096 -nodes -sha256 -keyout ${REGISTRY_CERTS_DIR}/domain.key -x509 -days 365 -out ${REGISTRY_CERTS_DIR}/domain.crt
+printf "${NC}"
+
+## How to make docker daemon trust docker registry
+printf "${BINFO}-----------------------------------------------------------------------------------------------------------------------${NC}\n"
+printf "${BINFO}  These notes help you to configure manually a docker engine to trust the docker registry that use the generated certs.${NC}\n"
+printf "${BINFO}  If your plan is to use the elements in this project (${ELEM}engine${BINFO} and ${ELEM}registry${BINFO}), you can ignore them.${NC}\n"
+printf "${BINFO}-----------------------------------------------------------------------------------------------------------------------${NC}\n"
+
+printf "\n${BINFO}NOTE I:${INFO} Use the generated certificate in a docker registry:${NC}\n"
+printf "\t${UINFO}- Copy the certificate and the key to the registry:\n"
+printf "\t\t${UINFO}· ${ELEM}${REGISTRY_CERTS_DIR}/domain.crt${UINFO} to ${ELEM}certs/domain.crt${UINFO}.${NC}\n"
+printf "\t\t${UINFO}· ${ELEM}${REGISTRY_CERTS_DIR}/domain.key${UINFO} to ${ELEM}certs/domain.key${UINFO}.${NC}\n"
+printf "\t${UINFO}- Set the following environment variables in the registry:\n"
+printf "\t\t${UINFO}· REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt\n"
+printf "\t\t${UINFO}· REGISTRY_HTTP_TLS_KEY=/certs/domain.key\n"
+printf "  ${INFO}The client must be able to resolve the name (which is configured in this script as ${ELEM}HOSTNAME_HOSTNAME=${ENGINE_HOSTNAME}${NC}) of the docker engine.${NC}\n"
+
+printf "\n${BINFO}NOTE II:${INFO} Instruct docker engine to trust registry:${NC}\n"
+printf "\t${UINFO}- Copy ${ELEM}${REGISTRY_CERTS_DIR}/domain.crt${UINFO} to ${ELEM}/etc/docker/certs.d/myregistrydomain.com:5000/ca.crt${UINFO}.${NC}\n"
+printf "\t${UINFO}- Restart docker engine.${NC}\n"
+
+printf "${BINFO}-----------------------------------------------------------------------------------------------------------------------${NC}\n\n"
