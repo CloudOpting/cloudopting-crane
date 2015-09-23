432807078fb9165bd67ce465fe1aab05ac417755
j-fuentes <jotaefe555555@gmail.com>
2015-09-23 14:09:42 +0200

    Test build base image

--- tests/testrunner/src/operations.py
+++ tests/testrunner/src/operations.py
@@ -3,7 +3,7 @@ import requests as req
 import settings as s
 
 def postBase(name, dockerfilePath):
-    dockerfile = open(dockerfilePath=,'rb')
+    dockerfile = open(dockerfilePath,'rb')
     return req.post(s.COMMANDER_URL+'/builder/images/bases', \
            data = {'imageName':name}, \
            files = {'dockerfile':dockerfile})
@@ -13,4 +13,18 @@ def getBase(name):
 
 def getRegistryImage(name):
     return req.get(s.REGISTRY_URL+'/v2/'+name+'/tags/list', \
-            verify='/var/tests/certs/registry-ca.crt')
+            verify=s.REGISTRY_CA)
+
+def purgeCrane():
+    return req.get(s.COMMANDER_URL+'/extra/purge')
+
+def assertStatusCode(res, statusCode, text='NOT GOOD'):
+    assert res.status_code == statusCode, \
+        res.request.method+": "+res.request.path_url+": "+ \
+        text + ": HTTP status code returned is "+ str(res.status_code) + \
+        ",different from " + str(statusCode)
+
+def assertStatus(res, desiredStatus, text='NOT GOOD'):
+    assert res.json()['status'] == desiredStatus, \
+        res.request.method+": "+res.request.path_url+": "+ \
+        text + ": 'status' returned is different from '"+ desiredStatus +"'"
--- tests/testrunner/src/tests.py
+++ tests/testrunner/src/tests.py
@@ -21,51 +21,61 @@ class TestsBasic:
 
 class TestsCompleteCycle:
     """Complete cycle tests: build, save, and execution"""
+
+    @classmethod
+    def setup_class(cls):
+        # delete engine containers, images and crane data
+        res = op.purgeCrane()
+        errText = "Cannot purge Crane"
+        op.assertStatusCode(res, 200, errText)
+        op.assertStatus(res, 'finished', errText)
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
     def test_webapp(self):
         """Simple webapp in remote pre-provisioned docker host"""
 
         # Directory where resources are stored
         resources = os.path.join(s.TEST_RES, 'test-1')
 
+        # Name of the image
+        imageName = 'ubuntubase'
+
+        # make sure that image do not exist in registry
+        res = op.getRegistryImage(s.DEF_BASE_PROVIDER+'/'+imageName)
+        #op.assertStatusCode(res, 404, \
+        #    "Cannot assure base image is not already on the registry.")
+
         # build base
-        res = op.buildBase('ubuntubase', \
-                os.path.join(resources, 'bases/ubuntubase'))
+        res = op.postBase(imageName, \
+                os.path.join(resources, 'bases/'+imageName))
+        errText = "Error in build base request"
+        op.assertStatusCode(res, 200, errText)
+        op.assertStatus(res, 'building', errText)
 
-        assert res.status_code == 200, \
-            res.request.method+": "+res.request.path_url+": "+ \
-            "HTTP status code returned is different from 200"
-
-        assert res.json()['status'] == 'building', \
-            res.request.method+": "+res.request.path_url+": "+ \
-            "'status' returned is different from 'building'"
-
-        while True: # wait until image is finished
-            res = op.getBase(name)
-            assert res.status_code == 200, \
-                res.request.method+": "+res.request.path_url+": "+ \
-                "HTTP status code returned is different from 200"
+        while True: # wait image built process to finish
+            res = op.getBase(imageName)
+            op.assertStatusCode(res, 200, "Error while asking base status")
 
             if res.json()['status'] == 'building':
                 time.sleep(5)
                 continue
 
-            assert res.json()['status'] == 'finished', \
-                res.request.method+": "+res.request.path_url+": "+ \
-                "'status' returned is different from 'finished'"
+            op.assertStatus(res, 'finished', \
+                "Error in base image built proccess")
+
+            break
 
 
         # check base
-        res = op.getRegistryImage('cloudopting/ubuntubase')
-
-        assert res.status_code == 200, \
-            res.request.method+": "+res.request.path_url+": "+ \
-            "HTTP status code returned is different from 200."+ \
-            "Base image not saved to registry."
-
-        
-
+        res = op.getRegistryImage(s.DEF_BASE_PROVIDER+'/'+imageName)
+        op.assertStatusCode(res, 200, 'Base image not saved to registry')
 
         # build context
+
+
         # check context
         # build image 1
         # check image 1
