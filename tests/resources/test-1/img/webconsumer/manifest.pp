c5b5681bdc94591d73ff33e70adb1ec4e4c1033e
j-fuentes <jotaefe555555@gmail.com>
2015-09-23 17:21:07 +0200

    Added images for testing

---
+++ tests/resources/test-1/img/webconsumer/manifest.pp
@@ -0,0 +1,18 @@
+# Clone repo
+vcsrepo { '/usr/src/app':
+  ensure   => present,
+  provider => git,
+  source   => 'git://github.com/wtelecom/crane-tests-webconsumer.git',
+}
+
+# Install requirements
+
+class { 'python' :
+  version    => 'system',
+  pip        => 'present',
+  dev        => 'present',
+}
+
+python::requirements { '/usr/src/app/requirements.txt' :
+  owner      => 'root',
+}
